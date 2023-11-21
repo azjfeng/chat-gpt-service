ac31291f9ccf2d1861c62110b1b0da87c95b1ba0
jameinfeng <jameinfeng@tencent.com>
2023-11-21 21:44:22 +0800

    update

--- src/module/chatText/chatText.controller.ts
+++ src/module/chatText/chatText.controller.ts
@@ -1,12 +1,61 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, Req } from '@nestjs/common';
 import { ChatTextService } from './chatText.service';
+import { OpenAI } from 'openai';
+import { Response } from 'express';
+interface TextBody {
+  prompt: string;
+}
 
 @Controller('/api/text')
 export class ChatTextController {
-  constructor(private readonly appService: ChatTextService) {}
+  private readonly openaiService;
+  constructor(private readonly chatTextService: ChatTextService) {
+    this.openaiService = new OpenAI({
+      apiKey: '',
+    });
+  }
 
-  @Post('getHello')
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('getGenerate')
+  async getGenerate(@Body() body: TextBody): Promise<string> {
+    const prompt = body.prompt;
+    const data = await this.chatTextService.getGenerate(prompt);
+    return data;
+  }
+  @Get('getGenerateStream')
+  async getGenerateStream(
+    @Body() body: TextBody,
+    @Res() res,
+    @Req() req,
+  ): Promise<any> {
+    console.log('body', body);
+    // 设置响应头，指定为流式数据
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const prompt = body.prompt;
+    // 当客户端断开连接时停止传输
+    req.on('close', () => {
+      console.log('asdsds');
+      res.end();
+    });
+    const stream = await this.openaiService.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: 'say this is text' }],
+      stream: true,
+    });
+    for await (const chunk of stream) {
+      console.log(chunk);
+      res.write(JSON.stringify(chunk));
+    }
+    // const intervalId = setInterval(() => {
+    //   const data = { message: 'This is a test message' };
+    //   res.write(`data: ${JSON.stringify(data)}\n\n`);
+    // }, 1000);
+
+    // req.on('close', () => {
+    //   clearInterval(intervalId);
+    //   res.end();
+    // });
   }
 }
--- src/module/chatText/chatText.service.ts
+++ src/module/chatText/chatText.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { OpenAI } from 'openai';
 
 @Injectable()
 export class ChatTextService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly openaiService;
+  constructor() {
+    this.openaiService = new OpenAI({
+      apiKey: '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',
+    });
+  }
+  async getGenerate(prompt: string): Promise<string> {
+    const response = await this.openaiService.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: prompt }],
+      temperature: 0.7,
+    });
+    console.log(response.choices[0].message.content);
+    return response;
+  }
+  async getGenerateStream(prompt: string) {
+    const stream = await this.openaiService.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: prompt }],
+      stream: true,
+    });
+    for await (const chunk of stream) {
+      console.log(chunk);
+      process.stdout.write(chunk.choices[0]?.delta?.content || '');
+    }
+    return 'aaaa';
   }
 }
